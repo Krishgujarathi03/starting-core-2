74a7037e0e47770fa35b0bbd564fd6ee542bf1cc
Mayank Deep <rishusinha525@gmail.com>
2025-05-10 00:38:06 +0530

    changed the contents of landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -13,20 +13,11 @@ const LandingPage = () => {
             We work with labels and brands to craft viral campaigns.
           </h2>
           <p className="text-base sm:text-lg font-lora text-gray-300 mb-4">
-            And in our free time, we solve real-world problems —<br />
+            And in our free time, we solve real-world problems<br />
             <span className="text-gray-400">
               because we can, and because we love it.
             </span>
           </p>
-
-          <div className="flex items-center justify-center md:justify-start space-x-3 mt-6">
-            <button className="w-14 h-14 sm:w-16 sm:h-16 bg-transparent border border-white hover:bg-red-600 hover:text-white text-white rounded-full flex items-center justify-center text-xl">
-              →
-            </button>
-            <span className="text-white hover:text-red-600 text-base sm:text-lg">
-              See More
-            </span>
-          </div>
         </div>
 
         {/* Right Column */}
